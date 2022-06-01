2b63613e91596dc4dc5c195125a8eb8e6d21f9ce
Oscar Iván Cujcuj López <88805642+ocujcuj2020271@users.noreply.github.com>
2022-05-31 23:16:41 -0600

    Intento de diseño

---
+++ src/App.tsx
@@ -0,0 +1,111 @@
+import Login from './components/Auth/login';
+import Informacion from './components/Home/home'
+import Error404 from './components/Errors/error404';
+import {
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { useState } from 'react';
+/**
+ * Paleta de Colores
+ * https://coolors.co/f1fffa-ccfccb-96e6b3-03b1bb-258db0-464e47
+ */
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#03b1bb',
+      contrastText: '#f1fffa',
+      light: '#6ee4ea',
+      dark: '#027379',
+    },
+    secondary: {
+      main: 'rgba(220,10,84,0.94)',
+      contrastText: '#f1fffa',
+      dark: '#b70042',
+      light: '#e23974',
+    },
+    divider: '#adbebf',
+    error: {
+      main: '#e84135',
+      contrastText: '#f1fffa',
+    },
+    warning: {
+      main: '#f1940a',
+      contrastText: '#2c3531',
+    },
+    info: {
+      main: '#2092ec',
+      contrastText: '#f1fffa',
+    },
+    success: {
+      main: '#50b554',
+      contrastText: '#f1fffa',
+    },
+    text: {
+      primary: '#464e47',
+      secondary: '#798c7c',
+      disabled: '#9a9a9a',
+    },
+    background: {
+      default: '#F1FFFA',
+    },
+  },
+  typography: {
+    fontSize: 12,
+  },
+  shape: {
+    borderRadius: 10,
+  },
+  spacing: 8,
+  transitions: {
+    duration: {
+      shortest: 150,
+      shorter: 200,
+      short: 550,
+      // most basic recommended timing
+      standard: 300,
+      // this is to be used in complex animations
+      complex: 375,
+      // recommended when something is entering screen
+      enteringScreen: 400,
+      // recommended when something is leaving screen
+      leavingScreen: 250,
+    },
+  },
+});
+/**
+ * Importante el uso de types para validar el tipo de dato o funcion
+ * que se maneja!!
+ */
+export type UserProps = {
+  user: User;
+  setUser: (user: User) => void;
+};
+type User = {
+  user: string;
+  password: string;
+};
+function App() {
+  
+
+  const [user, setUser] = useState<User>({
+    user: '',
+    password: ''
+  });
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes>
+        <Route path="/" element={<Login user={user} setUser={setUser} />} />
+        <Route path="/i" element={<Informacion/>}/>
+        <Route path="*" element={<Navigate to="/resource_not_found" replace />} />
+        <Route path="/resource_not_found" element={<Error404 />} />
+      </Routes>
+    </ThemeProvider>
+
+  );
+}
+
+export default App;
---
+++ src/components/Auth/login.tsx
@@ -0,0 +1,101 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+//import Types 
+import { UserProps } from '../../App'
+
+function Copyright(props: any) {
+    return (
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+            {'Copyright © '}
+            <Link color="inherit" href="https://traeguate.gt/">
+                Traeguate
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+/**
+ * Función de Login usando useState()
+ * ERROR AQUI EN LA LINEA 26, AVERIGUAR COMO PASAR UNA FUNCION COMO PARAMETRO EN TYPESCRIPT
+ */
+
+const Login = ({ user, setUser }: UserProps) => {
+
+    const handleInputChange = (event: { target: { name: any; value: any; }; }) => {
+        setUser({
+            ...user!,
+            [event.target.name]: event.target.value
+        });
+    }
+    const enviarDatos = (event: { preventDefault: () => void; currentTarget: HTMLFormElement | undefined; }) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        if (data) {
+            console.log({
+                user: data.get('user'),
+                password: data.get('password'),
+            });
+        }
+    }
+
+    return (
+        <React.StrictMode>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline />
+                <Box
+                    sx={{
+                        marginTop: '20vh',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h5" sx={{ color: '#464E47' }}>
+                        Administración
+                    </Typography>
+                    <Box component="form" onSubmit={enviarDatos} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="user"
+                            label="Usuario"
+                            name="user"
+                            onChange={handleInputChange}
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Contraseña"
+                            type="password"
+                            id="password"
+                            onChange={handleInputChange}
+                            autoComplete="current-password"
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2, pt: 1, pb: 1, boxShadow: 5 }}
+                        >
+                        Ingresar
+                        </Button>
+                    </Box>
+                </Box>
+                <Copyright sx={{ mt: 8, mb: 4 }} />
+            </Container>
+        </React.StrictMode>
+    );
+}
+
+export default Login;
